88db0e0ffbac020c4c4fdda010e4c7b997adeba7
Sravanthiteppala <168328082+Sravanthiteppala@users.noreply.github.com>
2025-05-30 12:29:11 +0530

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -20,6 +20,37 @@ INSERT INTO Departments (department_id, department_name) VALUES
 (10, 'HR'),
 (20, 'Engineering'),
 (40, 'Marketing');
+--outputs🗂️ Given Tables
+Employees
+employee_id	name	department_id
+1	Alice	10
+2	Bob	20
+3	Charlie	NULL
+4	David	30
+
+Departments
+department_id	department_name
+10	HR
+20	Engineering
+40	Marketing
+
+✅ Output of the Combined Query
+join_type	employee_name	department_name
+INNER JOIN	Alice	HR
+INNER JOIN	Bob	Engineering
+LEFT JOIN	Alice	HR
+LEFT JOIN	Bob	Engineering
+LEFT JOIN	Charlie	NULL
+LEFT JOIN	David	NULL
+RIGHT JOIN	Alice	HR
+RIGHT JOIN	Bob	Engineering
+RIGHT JOIN	NULL	Marketing
+FULL JOIN	Alice	HR
+FULL JOIN	Bob	Engineering
+FULL JOIN	Charlie	NULL
+FULL JOIN	David	NULL
+FULL JOIN	NULL	Marketing
+
 
 -- Insert data into Employees
 INSERT INTO Employees (employee_id, name, department_id) VALUES
